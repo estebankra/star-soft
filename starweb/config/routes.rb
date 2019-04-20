9c4f4b10d250fabd192c92dee1267a72698a354b
estebankra <esteban98alejandro@gmail.com>
2019-04-20 08:10:31 -0400

    Colores, devise, primeras vistas

--- starweb/config/routes.rb
+++ starweb/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :supplies
   resources :sponsors
+  resources :colors
+  devise_for :users
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
