d527a0b8be893746c422db07e9955f597104e21e
Esteban Krauwezuk <esteban98alejandro@gmail.com>
2019-05-28 21:20:37 -0400

    Cambios en vistas

--- starweb/app/controllers/reports_controller.rb
+++ starweb/app/controllers/reports_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class ReportController < ApplicationController
   before_action :authenticate_user!
 
   def index
--- starweb/config/routes.rb
+++ starweb/config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :colors
   devise_for :users
 
-  get 'home/index'
+  get 'reports/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'orders#index'
 end
