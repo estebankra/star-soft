28d1a0ff203a200ae1dd5469d0e5ec85d43b7482
estebankra <esteban98alejandro@gmail.com>
2019-08-06 15:04:47 -0400

    Added gem simple_form and improvements in form for products

--- starweb/app/models/product.rb
+++ starweb/app/models/product.rb
@@ -9,6 +9,11 @@ class Product < ApplicationRecord
   after_create :save_supplies
   after_update :save_supplies
 
+  # Validation
+  validates :name, :text, presence: true
+  validates :price, :decimal, presence: true
+  # validates :text, length: { minimum: 40 }
+
   has_attached_file :image, styles: { medium: '600x300', thumb: '300x150', mini: '200x100' }
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\Z}
 
