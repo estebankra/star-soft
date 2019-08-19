9fd5891742a9025fbb2d042cbebf0dcb1a265d03
estebankra <esteban98alejandro@gmail.com>
2019-08-19 13:59:50 -0400

    Add reports about invoices

--- starweb/app/services/reports_methods.rb
+++
@@ -1,6 +0,0 @@
-class ReportsMethods
-  # { 'Bernice' => 191, 'Carlee' => 160, 'Edison' => 60, ... }
-  def self.orders_by_client(properties)
-    Order.joins(:client).group('client.first_name').order('client.first_name').count
-  end
-end
