fbbd010194632c39e5fe2e2469cd09fcf9785835
estebankra <esteban98alejandro@gmail.com>
2019-08-21 23:58:21 -0400

    Configure app for unit tests and tests with selenium

--- starweb/app/controllers/orders_controller.rb
+++ starweb/app/controllers/orders_controller.rb
@@ -1,9 +1,7 @@
 class OrdersController < ApplicationController
   require 'httparty'
-
   before_action :authenticate_user!
   before_action :authenticate_secretary!, only: [:new, :edit, :create, :update, :destroy, :invoice, :pay]
-  before_action :authenticate_team!, only: [:state]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
